668c345a1a95baba8370876e24ce35f7b9167daa
“Tyler <tylerpalef@gmail.com>
2018-08-02 13:41:26 -0400

    trying to upload crm project. forked from david due to issues with managing DB

---
+++ crm.rb
@@ -0,0 +1,34 @@
+require_relative 'contact'
+require 'sinatra'
+
+get '/' do
+  redirect to '/home'
+end
+
+get '/home' do
+  erb :index
+end
+
+get '/contacts' do
+  @contacts = Contact.all
+  erb :contacts
+end
+
+get '/contacts/:id' do
+  #params[:id] has the id from the URL
+  # instructions for how to handle requests to this route will go here
+  @contact = Contact.find_by({id: params[:id].to_i})
+  if @contact
+    erb:show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+get '/about' do
+  erb :about
+end
+
+after do
+  ActiveRecord::Base.connection.close
+end
