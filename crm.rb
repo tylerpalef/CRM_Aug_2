4ce34ffeda35e5f5a884a833857ad9326a8c2e83
“Tyler <tylerpalef@gmail.com>
2018-08-03 15:56:32 -0400

    finished being able to edit contacts

--- crm.rb
+++ crm.rb
@@ -15,6 +15,31 @@ get '/contacts' do
   erb :contacts
 end
 
+get '/contacts/:id/edit' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
 get '/new' do
   erb :new
 end
@@ -35,6 +60,22 @@ get '/contacts/:id' do
   @contact = Contact.find_by({id: params[:id].to_i})
   if @contact
     erb:show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+
+    redirect to('/contacts')
   else
     raise Sinatra::NotFound
   end
